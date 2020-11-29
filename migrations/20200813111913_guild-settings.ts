96282f9da64d0ce7ecb1b0643befb7e39472c776
ChinoLilDemon <luna@aciralith.com>
2020-11-29 03:06:48 +0100

    database fix number 2

--- migrations/20200813111913_guild-settings.ts
+++ migrations/20200813111913_guild-settings.ts
@@ -3,21 +3,21 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema
-        .createTableIfNotExists('guild_prefix', tbl => {
+        .createTable('guild_prefix', tbl => {
             tbl.string('id').primary(); // guild id
             tbl.string('prefix').notNullable();
         })
-        .createTableIfNotExists('ban_log_channel', tbl => {
+        .createTable('ban_log_channel', tbl => {
             tbl.string('guild');
             tbl.string('channel');
             tbl.primary(['guild', 'channel'])
         })
-        .createTableIfNotExists('kick_log_channel', tbl => {
+        .createTable('kick_log_channel', tbl => {
             tbl.string('guild');
             tbl.string('channel');
             tbl.primary(['guild', 'channel']);
         })
-        .createTableIfNotExists('msg_log_channel', tbl => {
+        .createTable('msg_log_channel', tbl => {
             tbl.string('guild');
             tbl.string('channel');
             tbl.primary(['guild', 'channel']);
--- migrations/20201121231820_leveling.ts
+++ migrations/20201121231820_leveling.ts
@@ -3,19 +3,19 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema
-        .createTableIfNotExists('global_level', tbl => {
+        .createTable('global_level', tbl => {
             tbl.string('user').primary();
             tbl.integer('xp').notNullable();
             tbl.integer('level').notNullable();
         })
-        .createTableIfNotExists('local_level', tbl => {
+        .createTable('local_level', tbl => {
             tbl.string('user').notNullable();
             tbl.string('guild').notNullable();
             tbl.integer('xp').notNullable();
             tbl.integer('level').notNullable();
             tbl.primary(['user', 'guild'])
         })
-        .createTableIfNotExists('level_announce_channel', tbl => {
+        .createTable('level_announce_channel', tbl => {
             tbl.string('guild').primary();
             tbl.string('channel').notNullable();
         })
